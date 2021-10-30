778f524ad74d1aad1e3821a7daf7c4157aee2df0
Iwona Maraszkiewicz <ikonopinska@gmail.com>
2021-10-30 19:12:58 +0200

    Fix Departments tests

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ app.use((req, res) => {
 })
 
 // connects our backend code with the database
-const dbURI = process.env.NODE_ENV === 'production' ? 'url to remote db' : 'url to local db';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+if(NODE_ENV === 'production') dbUri = 'url to remote db';
+else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/companyDBtest1';
+else dbUri = 'mongodb://localhost:27017/companyDB';
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
@@ -34,4 +38,4 @@ const server = app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
 
-module.exoprts = server;
+module.exports = server;
